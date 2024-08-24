4d5c398792a6582cbb33072715874947f294a3e4
souleater04 <parthshresth04.k@gmail.com>
2024-08-24 12:17:29 +0530

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import MapComponent from './MapComponent.jsx';
 import ArduinoSimulation from './ArduinoSimulation.jsx';
 import DisplayBin from './DisplayBin.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <DisplayBin />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/map">Map</Link>
+            </li>
+            <li>
+              <Link to="/arduino">Arduino Simulation</Link>
+            </li>
+            <li>
+              <Link to="/display-bin">Display Bin</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/map" element={<MapComponent />} />
+          <Route path="/arduino" element={<ArduinoSimulation />} />
+          <Route path="/display-bin" element={<DisplayBin />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
